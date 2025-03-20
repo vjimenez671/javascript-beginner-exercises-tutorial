ce70df566df0e0f7d1743f7f84e549af3aea82fd
Vicente Jiménez <vicentejimenez.actor@gmail.com>
2025-03-20 00:17:56 +0000

    listo

--- exercises/24-Javascript_Objects/app.js
+++ exercises/24-Javascript_Objects/app.js
@@ -16,20 +16,9 @@ var person2 = {
     significantOther: person
 };
 
-
-
 var family = {
     lastName: "Doe",
-    members: [person, person2,]   //Array of objects, don't forget to add Jimmy
-};
-
-var person3 = {
-    name: "Jimmy",
-    lastName: Doe,
-    age: 13,
-    gender: "male",
-    luckyNumbers: [1, 2, 3, 4]
-    significantOther: null
+    members: [person, person2]   //Array of objects, don't forget to add Jimmy
 };
 
 
@@ -39,13 +28,36 @@ function addAllFamilyLuckyNumbers(anArray){
   //To-Do: loop and add; consider nested loops
   //Hint: use the anArray variable to get all of the lucky numbers
   
+  let members = anArray 
+  for (let person in members) { 
+      for (let j in members[person].luckyNumbers){ // [7, 11, 13, 17]
+            //sumOfAllLuckyNumbers = sumOfAllLuckyNumbers + members[person].luckyNumbers[j]
+            sumOfAllLuckyNumbers += members[person].luckyNumbers[j]
+            // console.log("lucky numbers = ", sumOfAllLuckyNumbers, " sumo ", members[person].luckyNumbers[j]);
+      }
+      
+  }
+  
+  
   return sumOfAllLuckyNumbers;
 }
 
 //Enter all your code here:
+var person3 = {
+    name: "Jimmy",
+    lastName: "Doe",
+    age: 13,
+    gender: "male",
+    luckyNumbers: [1, 2, 3, 4],
+    significantOther: null
+}
 
-family.members.push(person3)
+family.members.push(person3) 
+
+person.luckyNumbers.splice(3, 1, 33)
 
 
 //Do not make changes below:
 console.log(addAllFamilyLuckyNumbers(family.members)); 
+
+//family.members contiene a las tres person
